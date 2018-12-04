2a652c1e6338c5b6d1eacec336c69c1614590356
Eddie Webb <ollitech@gmail.com>
2018-12-04 09:31:21 -0500

    scan for keywords, wait for losing message

--- cypress/integration/amazon_giveaway_spec.js
+++ cypress/integration/amazon_giveaway_spec.js
@@ -1,45 +1,64 @@
-Cypress.Cookies.defaults({
-  whitelist: /session|remember/
-})
+
 
 
 
 
 describe('Win All The Things', function() {
 
-	it('can find giveaways page', function() {
-		cy.visit('https://smile.amazon.com/ga/giveaways')
-	})
 	it('can generate new links', function() {
-				//
-		// Collect links from all pages
-		//
-		cy.visit('https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_youraccount_signout%26signIn%3D1%26useRedirectOnSuccess%3D1')
-		cy.get('#ap_email').type('ollitech@gmail.com')
-		cy.get('#ap_password').type('')
-		cy.get('label > input').check()
-		//cy.get('#signInSubmit').click()
-		cy.pause()
-
-
+		var hitLinks=0
 		var allLinks = []
-		cy.visit('https://smile.amazon.com/ga/giveaways')
-		var start = 7
-		var batch = 10
+		var start = parseInt(Cypress.env('start_page'))
+		var batch = parseInt(Cypress.env('page_count'))
+		var yesWords = Cypress.env('yes_words').split(',')
 		var genArr = Array.from({length:batch},(v,k)=>k+start)
 		cy.wrap(genArr).each((index) => {
+			//collect all links from curent page
 			allLinks[index] = []
-			cy.visit('https://smile.amazon.com/ga/giveaways?pageId=' + index)
+			cy.visit('/ga/giveaways?pageId=' + index)
 			cy.get('#giveaway-grid>.giveawayItemContainer>.giveAwayItemDetails').each(($el, mindex, $list) => {
-				allLinks[index].push( $el[0].href )
+				cy.wrap($el).within(($summary) =>{
+						cy.get('.a-size-base').then(($link) =>{
+							var description = $link[0].outerHTML
+							//cy.log($link)
+							cy.log(description)
+							var pattern = new RegExp(yesWords.join("|"),'i')
+							var matches;
+							if ((matches = pattern.exec(description)) != null) {
+							   allLinks[index].push( $el[0].href )
+								cy.log("matched: " + matches[0])
+							}
+						})
+				})
+				
 			})
-			cy.log(allLinks[index])
+
 			cy.writeFile('links'+index+'.json',allLinks[index])
-			cy.wait(1000)
+
+			//hit all page givewyas
 			cy.readFile('links'+index+'.json').each((item)=>{
-				cy.visit(item)
+				//log in
+				if(hitLinks%20 == 0){
+					cy.visit('/gp/flex/sign-out.html/ref=nav_youraccount_signout?ie=UTF8&action=sign-out&path=%2Fgp%2Fyourstore%2Fhome&signIn=1&useRedirectOnSuccess=1')
+					cy.clearCookies()
+					cy.wait(1500)
+					cy.visit('/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_youraccount_signout%26signIn%3D1%26useRedirectOnSuccess%3D1')
+					cy.get('#ap_email').type('ollitech@gmail.com')
+					cy.get('#ap_password').type(Cypress.env('password'))
+					cy.get('label > input').check()
+					cy.get('#signInSubmit').click()
+				}
+				hitLinks++
+				cy.log(item)
+				cy.log(hitLinks)
+				
+
+				//visit page
+				cy.visit(item)  // +urlObject.search
 				cy.wait(500)
-				cy.get('#giveaway-page-content').then(($body) =>{
+
+				//giveaway page loaded, decide what action to take
+				cy.get('#a-page').within(($body) =>{
 					 var asString = $body[0].outerHTML
 
 					 if (asString.includes('id="giveaway-default-return-link-button"')){
@@ -48,112 +67,58 @@ describe('Win All The Things', function() {
 					 }else if (asString.includes('Giveaway ended')){
 						//cy.screenshot('entered'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewpoint"})
 						cy.log("Expired " + item)
-					 }else if (asString.includes('<input name="subscribe"')){
-						//cy.screenshot('entered'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewpoint"})
-						cy.get('input[name="subscribe"]').click()
-						cy.log("Subscribe: " + item)
-						cy.get('input.a-button-input',{"timeout":30000})
-						cy.screenshot('page-' + index + '/subscribe'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})	
-					 }else if(asString.includes('id="box_click_target"')) {
-					    //magic box was cfond click it
-						cy.get('#box_click_target').click()
-						cy.get('input.a-button-input',{"timeout":30000})
-						cy.screenshot('page-' + index + '/boxed'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})	
-						//cy.screenshot()
-					  }else if (asString.includes('id="youtube-iframe"')) {
-					    //video box, play it
-						cy.get('#youtube-iframe').click()
-						cy.get('input[name="continue"]',{"timeout":30000}).click({"timeout":30000})
-						cy.wait(2000)
-						cy.get('input.a-button-input',{"timeout":30000})
-						cy.screenshot('page-' + index + '/yt'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})				  
-					  }else if (asString.includes('<video')) {
-					    //video box, play it
-						cy.get('#giveaway-social-container').click()
-						cy.get('input[name="continue"]',{"timeout":30000}).click({"timeout": 30000})
-						cy.wait(2000)
-						cy.get('input.a-button-input',{"timeout":30000})
-						cy.screenshot('page-' + index + '/airy'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})	
-					  }else{
-					  	//no idea, screenshot
-					  	//cy.wait(10000)
-						cy.get('input.a-button-input',{"timeout":30000}).click({"timeout":30000})
-						cy.wait(2000)
-						cy.get('input.a-button-input',{"timeout":30000})
-					  	cy.screenshot('page-' + index + '/guess'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})
-					  }
-					  var updatedContents = $body[0].outerHTML
-					  if(updatedContents.includes("won") || updatedContents.includes("winner")|| updatedContents.includes("congratulations")){
-					  	cy.log("winner - " + item)
-					  	cy.screenshot('Winner'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})
-					  	cy.pause()
+					 }else{
+					 	if (asString.includes('<input name="subscribe"')){
+							//cy.screenshot('entered'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewpoint"})
+							cy.wait(600)
+							cy.log("Subscribe: " + item)
+							cy.get('input[name="subscribe"]').click()
+							cy.get('input.a-button-input')
+						 }else if(asString.includes('id="box_click_target"')) {
+							cy.log("Box: " + item)
+						    //magic box was cfond click it
+							cy.wait(1500)
+							cy.get('#giveaway-social-container').click()
+							cy.get('#box_click_target').click()
+							cy.get('input.a-button-input')
+							//cy.screenshot()
+						  }else if (asString.includes('id="youtube-iframe"')) {
+							cy.log("Youtube: " + item)
+						    //video box, play it
+							cy.get('#youtube-iframe').click()
+							cy.get('input[name="continue"]').click()
+							cy.wait(1000)						
+							cy.get('#box_click_target').dblclick()
+							cy.get('input.a-button-input')		  
+						  }else if (asString.includes('<video')) {
+							cy.log("VIdeo: " + item)
+							cy.get('#giveaway-social-container').click()
+							cy.wait(1000)
+							cy.get('input[name="continue"]').click()
+							cy.get('input.a-button-input')
+						  }else{
+							cy.log("Guess: " + item)
+							cy.get('input.a-button-input').click()
+							cy.get('input.a-button-input')
+											  	
+						  }
+						  cy.get('#title').then(($message) =>{
+								var updatedContents = $message[0].outerHTML
+								if(! updatedContents.includes("you didn't win")  && ! updatedContents.includes("Giveaway ended") ){
+									cy.log("winner - " + item)
+									cy.screenshot('Winner'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"fullPage"})
+									cy.pause()
+								}	
+							})
+					 } 
+				})		
 
-					  }
-				})			
+				
 			})
-			cy.wait(200)
+			// wait 5 seconfs between full page scrapes.
+			cy.wait(1000)
 		})
 	})
-
-
-
-	//it('can enter giveaways', function() {
-	//	//cy.visit('https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_youraccount_signout%26signIn%3D1%26useRedirectOnSuccess%3D1')
-	//	//cy.get('#ap_email').type('ollitech@gmail.com')
-	//	//cy.get('#ap_password').type('')
-	//	//cy.get('label > input').check()
-	//	//cy.get('#signInSubmit').click()
-	//	cy.wait(2000)
-	//	
-	//	//
-	//	// Visit each, discern type and enter  
-	//	// a-button-input
-	//	//
-	//	cy.readFile('links.json').each((item)=>{
-	//		cy.log(item)
-	//		cy.visit(item)
-	//		cy.get('.participation-action-item').then(($body) =>{
-	//			 var asString = $body[0].outerHTML
-//
-	//			 if (asString.includes('id="giveaway-default-return-link-button"')){
-	//				//cy.screenshot('entered'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewpoint"})
-	//				cy.log("Already Entered: " + item)
-	//			 }else if (asString.includes('<input name="subscribe"')){
-	//				//cy.screenshot('entered'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewpoint"})
-	//				cy.get('input[name="subscribe"]').click()
-	//				cy.log("Subscribe: " + item)
-	//				cy.screenshot('subscribe'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewport"})	
-	//			 }else if(asString.includes('id="box_click_target"')) {
-	//			    //magic box was cfond click it
-	//				cy.get('#box_click_target').click()
-	//				cy.wait(5000)
-	//				cy.get('input.a-button-input')
-	//				cy.screenshot('boxed'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewport"})	
-	//				//cy.screenshot()
-	//			  }else if (asString.includes('id="youtube-iframe"')) {
-	//			    //video box, play it
-	//				cy.get('#youtube-iframe').click()
-	//				cy.get('input[name="continue"]').click({"timeout":30000})
-	//				cy.wait(5000)
-	//				cy.get('input.a-button-input')
-	//				cy.screenshot('yt'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewport"})				  
-	//			  }else if (asString.includes('<video')) {
-	//			    //video box, play it
-	//				cy.get('#giveaway-social-container').click()
-	//				cy.get('input[name="continue"]').click({"timeout":30000})
-	//				cy.wait(5000)
-	//				cy.get('input.a-button-input')
-	//				cy.screenshot('airy'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewport"})	
-	//			  }else{
-	//			  	//no idea, screenshot
-	//			  	//cy.wait(10000)
-	//			  	cy.screenshot('misses'+ item.replace(/[^a-z0-9]/gi, '_'),{"capture":"viewport"})
-	//			  }
-	//		})			
-//
-	//		cy.wait(200)
-	//	})
-	//})
 })
 
 
