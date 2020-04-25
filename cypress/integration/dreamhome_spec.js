084caaaaba1d6fc026a4cb042fbb65951ba2aff2
Eddie Webb <ollitech@gmail.com>
2020-04-25 06:47:57 -0400

    smarthome

--- cypress/integration/dreamhome_spec.js
+++ cypress/integration/dreamhome_spec.js
@@ -32,10 +32,10 @@ Cypress.Commands.add(
 
 describe('Enter @ DIY', function() {
 	it('can enter DIY', function() {
-		cy.wrap(['dubblabubbla@gmail.com','ollitech@gmail.com','edward.a.webb@gmail.com']).each( ($li, index, $lis) => {
-			cy.visit('https://www.diynetwork.com/hgtv-dream-home?ocid=direct')
+		cy.wrap(['dubblabubbla@gmail.com','dubbla.bubbla@gmail.com','ollitech@gmail.com','edward.a.webb@gmail.com','edwardawebb@gmail.com']).each( ($li, index, $lis) => {
+			cy.visit('https://www.diynetwork.com/hgtv-smart-home?ocid=direct')
 
-			cy.get('#ngxFrame163663')
+			cy.get('#ngxFrame171589')
 			  .iframeLoaded()
 			  .its('document').as('formcontent')
 
@@ -49,7 +49,7 @@ describe('Enter @ DIY', function() {
 			cy.wait(5000)
 
 
-			cy.get('#ngxFrame163663')
+			cy.get('#ngxFrame171589')
 			  .iframeLoaded()
 			  .its('document').as('formcontent2')
 			 cy.get('@formcontent2')
@@ -60,10 +60,10 @@ describe('Enter @ DIY', function() {
 
 	})
 	it('can enter HGTV', function() {		
-		cy.wrap(['dubblabubbla@gmail.com','ollitech@gmail.com','edward.a.webb@gmail.com']).each( ($li, index, $lis) => {
-			cy.visit('https://www.hgtv.com/design/hgtv-dream-home/sweepstakes?ocid=direct')
+		cy.wrap(['dubblabubbla@gmail.com','dubbla.bubbla@gmail.com','ollitech@gmail.com','edward.a.webb@gmail.com','edwardawebb@gmail.com']).each( ($li, index, $lis) => {
+			cy.visit('https://hgtv.com/sweepstakes/hgtv-smart-home/sweepstakes?ocid=direct')
 
-			cy.get('#ngxFrame163659')
+			cy.get('#ngxFrame171583')
 			  .iframeLoaded()
 			  .its('document').as('formcontent')
 
@@ -77,7 +77,7 @@ describe('Enter @ DIY', function() {
 			cy.wait(5000)
 
 
-			cy.get('#ngxFrame163659')
+			cy.get('#ngxFrame171583')
 			  .iframeLoaded()
 			  .its('document').as('formcontent2')
 			 cy.get('@formcontent2')
